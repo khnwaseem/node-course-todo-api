cf86224736e33dc12e941d40079a3ec2f899d9c2
worzett <khanwaseem094@gmail.com>
2019-03-18 19:51:39 +0530

    Add cases for test /todos/:id

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/todos/:id', (req,res) => {
 
 	Todo.findById(id).then((todo) => {
 		if(!todo) {
-			return res.send("Id is not found");
+			return res.status(404).send("Id is not found");
 		}
 	       res.send({todo});
 	}, (e) => {
